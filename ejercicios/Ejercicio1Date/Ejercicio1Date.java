66c164f2474054f0cc0138391656b0a78d4bdeb2
DiegoArteaga <diego.arteaga2101@alumnos.ubiobio.cl>
2022-03-17 22:55:01 -0300

    commit on Ejercicio1Date

--- ejercicios/Ejercicio1Date/Ejercicio1Date.java
+++ ejercicios/Ejercicio1Date/Ejercicio1Date.java
@@ -8,11 +8,12 @@ public class Ejercicio1Date {
         //[12,15] 4 bits mes
         //[16,20] 5 bits dia
         Scanner tec = new Scanner(System.in);
-        System.out.println("Ingrese dia: "); int dia = tec.nextInt();
-        System.out.println("Ingrese mes: "); int mes = tec.nextInt();
-        System.out.println("Ingrese año: "); int anio = tec.nextInt();
+        System.out.print("Ingrese dia: "); int dia = tec.nextInt();
+        System.out.print("Ingrese mes: "); int mes = tec.nextInt();
+        System.out.print("Ingrese año: "); int anio = tec.nextInt();
 
         setDia(dia);
+        setMes(mes);
 
     }
 
@@ -44,10 +45,25 @@ public class Ejercicio1Date {
         if (dia > 0 && dia <= 31) {
             int mask = 63488; //00000000000000001111100000000000
             data = (data & mask) | (dia << 16);
-            System.out.println("Data: " + Long.toBinaryString(data));
+            System.out.println("Data: " + Long.toBinaryString(data) + "\nFecha: " + dia + "\n");
         } else {
             System.out.println("Dia no ingresado");
         }
     }
 
+    public static void setMes(int mes) {
+        if (mes > 0 && mes <= 12) {
+            int mask = 983040; //00000000000011110000000000000000
+            data = (data & mask) | (mes << 17);
+            System.out.println("Data: " + Long.toBinaryString(data) + "\nMes: " + mes + "\n");
+        } else {
+            System.out.println("Mes no ingresado");
+        }
+    }
+
+    public static void setAnio(int anio) {
+        if (anio > 0 && anio <= 2022) {
+            //11111111111100000000000000000000
+        }
+    }
 }
