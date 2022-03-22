21e3e292ccc7e0b22dbb4184da66527734cc3ae2
DiegoArteaga <diego.arteaga2101@alumnos.ubiobio.cl>
2022-03-22 16:31:12 -0300

    commit on ejercicio1Date

--- ejercicios/Ejercicio1Date/Ejercicio1Date.java
+++ ejercicios/Ejercicio1Date/Ejercicio1Date.java
@@ -2,6 +2,8 @@ package ejercicios.Ejercicio1Date;
 
 import java.util.Scanner;
 
+import javafx.geometry.Side;
+
 public class Ejercicio1Date {
     
     private static int data;
@@ -25,8 +27,18 @@ public class Ejercicio1Date {
         return anio;
     }
 
-    //año      //mes //dia //vacio
-//000000000000 1111  11111 00000000000 
+    public int getHora() {
+        int hora = (data << 21) >>> 27;
+        return hora;
+    }
+
+    public int getMinuto() {
+        int minuto = (data << 26) >>> 25;
+        return minuto;
+    }
+
+    //año      //mes //dia //hora //minuto
+//111111111111 1111  11111 11111  111111 
 
     public static void setDia(int dia) { //deja solo los bits del dia
         if (dia > 0 && dia <= 31) {
@@ -39,7 +51,7 @@ public class Ejercicio1Date {
     }
 
     public static void setMes(int mes) {
-        if (mes > 0 && mes <= 15) {
+        if (mes > 0 && mes <= 12) {
             int mask = 983040 | (1 << 31); //00000000000011110000000000000000
             data = (data & mask) | (mes << 16);
             System.out.println("Data: " + Long.toBinaryString(data) + "\nMes: " + mes + "\n");
@@ -58,11 +70,31 @@ public class Ejercicio1Date {
         }
     }
 
+    public static void setHora(int hora) {
+        if (hora >= 00 && hora <= 23) {
+            int mask = 992 | (1 << 31); //0000000000000000000001111100000
+            data = (data & mask) | (hora << 21);
+            System.out.println("Data: " + Long.toBinaryString(data) + "\nHora " + hora + "\n");
+        } else {
+            System.out.println("Hora no ingresada");
+        }
+    }
+
+    public static void setMinuto(int minuto) {
+        if (minuto >= 0 && minuto <= 59) {
+            int mask = 63 | (1 << 31); //00000000000000000000000000111111
+            data = (data & mask) | (minuto << 26);
+            System.out.println("Data: " + Long.toBinaryString(data) + "\nMinuto " + minuto + "\n");
+        } else {
+            System.out.println("Minuto no ingresado");
+        }
+    }
+
     public String toString() {
         if (getMes()<10) {
-            return getDia() + "/0" + getMes() + "/" + getAnio();
+            return getDia() + "/0" + getMes() + "/" + getAnio() + "\nHora: " + getHora() + "\nMinuto: " + getMinuto();
         } else {
-            return getDia() + "/" + getMes() + "/" + getAnio();
+            return getDia() + "/" + getMes() + "/" + getAnio() + "\nHora: " + getHora() + "\nMinuto: " + getMinuto();
         }
     }
 }
--- ejercicios/Ejercicio1Date/Main.java
+++ ejercicios/Ejercicio1Date/Main.java
@@ -6,16 +6,20 @@ public class Main {
 
         Ejercicio1Date fechaDate = new Ejercicio1Date();
 
-        int dia, mes, anio;
+        int dia, mes, anio, hora, minuto;
 
         Scanner tec = new Scanner(System.in);
         System.out.print("Ingrese dia: "); dia = tec.nextInt();
         System.out.print("Ingrese mes: "); mes = tec.nextInt();
         System.out.print("Ingrese año: "); anio = tec.nextInt();
+        System.out.print("Ingrese hora: "); hora = tec.nextInt();
+        System.out.print("Inrgese minuto: "); minuto = tec.nextInt();
 
         fechaDate.setDia(dia);
         fechaDate.setMes(mes);
         fechaDate.setAnio(anio);
+        fechaDate.setHora(hora);
+        fechaDate.setMinuto(minuto);
         System.out.println(fechaDate.toString());
     }
 }
