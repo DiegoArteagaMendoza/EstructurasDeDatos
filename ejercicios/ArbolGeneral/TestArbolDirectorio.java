5a43e6d3f45146c74ec07c991799df2b72d17562
DiegoArteaga <diego.arteaga2101@alumnos.ubiobio.cl>
2022-04-15 14:15:28 -0400

    commit on ArbolDirectorio

---
+++ ejercicios/ArbolGeneral/TestArbolDirectorio.java
@@ -0,0 +1,34 @@
+public class TestArbolDirectorio {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArbolDirectorio ad = new ArbolDirectorio();
+		ad.print();
+		System.out.println("Current size: " + ad.size());
+		boolean chek = ad.crearCarpeta("root", "Carpeta 1");
+		if (chek == true) System.out.println("CARPETA CREADA");
+		chek = ad.crearCarpeta("root", "Carpeta 2");
+		if (chek == true) System.out.println("CARPETA CREADA");
+		chek = ad.crearCarpeta("root", "Carpeta 3");
+		if (chek == true) System.out.println("CARPETA CREADA");
+		ad.print();
+		System.out.println("Current size: " + ad.size());
+		ad.crearCarpeta("root/Carpeta 1", "Carpeta 4");
+		ad.crearCarpeta("root/Carpeta 2", "Carpeta 2");
+		ad.crearCarpeta("root/Carpeta 2", "Carpeta 5");
+		ad.crearCarpeta("root/Carpeta 2", "Carpeta 4");
+		ad.print();
+		System.out.println("Current size: " + ad.size());
+		ad.mover("root/Carpeta 2", "root/Carpeta 1");
+		ad.mover("root/Carpeta 2", "root/Carpeta 3");
+		ad.print();
+		System.out.println("Current size: " + ad.size());
+		ad.eliminarCarpeta("root/Carpeta 1");
+		ad.eliminarCarpeta("root/Carpeta 3/Carpeta 2");
+		ad.print();
+		System.out.println("Current size: " + ad.size());
+
+		System.out.println("\nPROCESO TERMINADO");
+	}
+
+}
