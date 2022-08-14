2f23897ba601c3b2b55c17db5030005c97dc969a
DiegoArteaga <diego.arteaga2101@alumnos.ubiobio.cl>
2022-08-14 15:17:06 -0400

    folders y archivos faltantes

---
+++ lab10/Parsing.java
@@ -0,0 +1,35 @@
+import java.util.Stack;
+
+/**
+ * 
+ * @author Martita
+ * 
+ * Para más info, visitar https://es.stackoverflow.com/questions/26907/infijo-a-posfijo-en-java
+ * Y http://informatica.uv.es/iiguia/AED/laboratorio/P6/pr_06_2005.html
+ *
+ */
+
+public class Parsing {
+	
+	public static String fromInfijoToPostfijo(String infijo) {
+		return null;
+	}
+	
+	private static boolean isANumber(char symbol) {
+		return symbol == '0' || symbol == '1' || symbol == '2' || symbol == '3' || 
+				symbol == '4' || symbol == '5' || symbol == '6' || symbol == '7' || 
+				symbol == '8' || symbol == '9';
+	}
+	
+	private static int prioridad(char symbol) {
+		if(symbol == '(') return 0;
+		if(symbol == '+' || symbol == '-') return 1;
+		if(symbol == '*' || symbol == '/') return 2;
+		return -1;
+	}
+
+	
+	public static ArbolExpresion getArbol(String postfijo) {
+		return null;
+	}
+}
