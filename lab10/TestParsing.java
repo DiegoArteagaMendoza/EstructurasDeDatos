1a157d554285ef2cdeed75ca848a43274ef76f0d
DiegoArteaga <diego.arteaga2101@alumnos.ubiobio.cl>
2022-08-14 15:25:03 -0400

    a

--- lab10/TestParsing.java
+++ lab10/TestParsing.java
@@ -11,11 +11,11 @@ public class TestParsing {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner tec = new Scanner(System.in);
-		System.out.print("Ingrese su expresión: ");
+		System.out.print("Ingrese su expresiï¿½n: ");
 		String expresion = tec.next();
 		tec.close();
 		String salida = Parsing.fromInfijoToPostfijo(expresion);
-		System.out.print("Su expresión postfija es: " + salida);
+		System.out.print("Su expresiï¿½n postfija es: " + salida);
 		ArbolExpresion a = Parsing.getArbol(salida);
 		System.out.println("\n\nSU ARBOL DE EXPRESION: \n");
 		a.print(0);
