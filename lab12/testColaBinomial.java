64bd097cae413cf51e050c4e4b9247e38d4a9689
DiegoArteaga <diego.arteaga2101@alumnos.ubiobio.cl>
2022-08-17 16:26:02 -0400

    a

--- lab12/testColaBinomial.java
+++ lab12/testColaBinomial.java
@@ -1,3 +1,20 @@
+//Diego Arteaga
+//Sebastian Jouannet
+
+// ------ EXPLICACION MINHEAP TO MAXHEAP ------
+/*
+Al usar la funcion BinomialHeapUnion (Clase ColaBinomial.java), 
+en el primer else (linea 73), se ve la comparación de valores 
+entre las claves del presente y el siguiente, la cual dice que si 
+el presente es menor, el hermano de este pasa a ser el
+siguiente, probocando un minHeap, en cambio, para realizar
+un maxHeap, tenemos que validar si el presente es mayor que 
+el siguiente, si se da esto, el presente queda primero,
+y en caso contrario el siguiente queda primero, de esta manera
+el arbol en ves de quedar con el menor dato en la parte 
+superior, queda el mayor en esta.
+*/ 
+
 class testColaBinomial{
 	public static void main(String[] args)
 	{
