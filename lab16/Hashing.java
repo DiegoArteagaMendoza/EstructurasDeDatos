121edbc8ff0d67a23c2bfb4879fdf748d880542e
DiegoArteaga <diego.arteaga2101@alumnos.ubiobio.cl>
2022-08-18 22:40:36 -0400

    finalizacion lab 16

--- lab16/Hashing.java
+++ lab16/Hashing.java
@@ -56,7 +56,17 @@ public class Hashing {
 		}
 		return false;
 	}
-	
+
+	// Implementar Funcion que calcula el factor de carga de un hashing
+	public double factorCarga() {
+		int cantidad = 0;
+		for(int i = 0; i < T.length; i++) {
+			if (T[i] != null) {
+				cantidad++;
+			}
+		}
+		return (double) cantidad / T.length;
+	}	
 	
 	int h(int k, int i) {
 		return (h1(k) + (i * h2(k))) % T.length;
@@ -111,3 +121,25 @@ public class Hashing {
 	}
 
 }
+
+
+/*
+	RESPUESTA A LAS PREGUNTAS:
+ * 	a)¿Que ocurre con las funciones de insercion, busqueda y eliminacion cuando el factor de 
+ * 		carga es cercano a 1?
+ * 
+ * 	  	R: Funcion insercion: a medida que el factor aumente, tambien aumentara el tiempo
+ * 			que se demora, esto ya que elmetodo tardara mas en buscar un espacio para la 
+ * 			insercion.
+ * 		   Funcion busqueda: a medida que el factor de carga aumente, el tiempo de busqueda
+ * 			lo hara tambien, ya que hay mas datos que revisar para buscar un nodo.
+ * 		   Funcion eliminacion: a medida que el factor de carga aumente, la funcion permanece igual.
+ * 
+ *  b)Describir los pasos a realizar para duplicar el tamaño de la tabla hash y reducir
+ * 		el factor de carga. ¿Para que valor de "alfa" es necesari para realizar el proceso?
+ * 
+ * 		R: el primer paso es realizar el metodo de insercion, el cual se comprbaria si el factor
+ * 			de carga es 1 tras la insercion del nodo mas reciente, en caso de ser asi, se duplicara la 
+ * 			tabla hash, ingresando el duplicado en un nuevo arreglo, con el doble de tamaño, debido a esto
+ * 			el valor siempre debe ser 1.
+ */
